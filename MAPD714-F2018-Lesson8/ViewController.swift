a17fce83a41880a7b9c30ca67efa22b0470bebc8
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2018-11-01 19:25:13 -0400

    Initial Commit

---
+++ MAPD714-F2018-Lesson8/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MAPD714-F2018-Lesson8
+//
+//  Created by Tom Tsiliopoulos on 2018-11-01.
+//  Copyright © 2018 CentennialCollege. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ MAPD714-F2018-Lesson8Tests/MAPD714_F2018_Lesson8Tests.swift
@@ -0,0 +1,34 @@
+//
+//  MAPD714_F2018_Lesson8Tests.swift
+//  MAPD714-F2018-Lesson8Tests
+//
+//  Created by Tom Tsiliopoulos on 2018-11-01.
+//  Copyright © 2018 CentennialCollege. All rights reserved.
+//
+
+import XCTest
+@testable import MAPD714_F2018_Lesson8
+
+class MAPD714_F2018_Lesson8Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
